885c9cad9be5b58cf644d3415a5afa3e6a9be9e3
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-02-02 10:29:38 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+
+int i,k,n,b=0,a[78];
+scanf("%d ",&n);
+
+
+printf(" the value of n is%d ",n);
+
+scanf("%d ",&k);
+
+printf(" the value of k is%d ",k);
+for(i=1;i<=n;i++)
+{
+	
+	printf(" array is %d",a[n]);
+}
+if(k!=0)
+{
+
+for(i=1;i<=k;i++)
+{
+	
+	b=b+a[k];
+}
+}
+
+
+printf("%d",b);
+
+	return 0;
+}
