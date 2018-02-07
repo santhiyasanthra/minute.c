19b93084a94c8184d7e63692355961fd4ca8833c
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-02-07 11:30:56 +0000

    Create stringg.c

---
+++ stringg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+int i,n;
+char name[23];
+scanf("%s",&name);
+scanf("%d",&n);
+printf("%d",n);
+
+for(i=0;i<=n;i++)
+{
+
+printf("%s\n",name);
+}
+return 0;
+}
+
+
