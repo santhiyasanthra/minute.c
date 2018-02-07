58810e553c879072fff820f70ec0a794cb06c4db
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-02-07 10:22:17 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the number in words %c",ch);
+scanf("%c",&ch);
+if(ch>='1'&&ch<='9')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
